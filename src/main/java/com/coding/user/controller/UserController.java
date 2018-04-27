915dcce7ccb7e6aea15bd3581fdad1e5325a3866
963559210@qq.com <963559210@qq.com>
2018-04-27 18:35:46 +0800

    可以了完美

--- src/main/java/com/coding/user/controller/UserController.java
+++ src/main/java/com/coding/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.coding.user.controller;
 
 import com.coding.user.entity.User;
 import com.coding.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +19,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @RequestMapping("/user")
     public String user(){
+        logger.info("hello");
         User user = new User();
+        user.setUsername("333");
         user.setPassword("cww123456");
         user.setMobile("15058547187");
         user.setRealName("程武武");
