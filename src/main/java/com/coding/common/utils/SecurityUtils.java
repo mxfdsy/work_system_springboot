1f8c1505284a8dab5003b6782bf1a9b322e6a42d
平凡的世界 <963559210@qq.com>
2018-04-29 10:12:25 +0800

    alert不行这是什么鬼

---
+++ src/main/java/com/coding/common/utils/SecurityUtils.java
@@ -0,0 +1,47 @@
+package com.coding.common.utils;
+
+import sun.misc.BASE64Encoder;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by 平凡的世界 on 2018/4/29.
+ */
+public class SecurityUtils {
+
+    /**
+     * 加密密码
+     * @param password 密码
+     * @return  加密后的密码
+     * @throws NoSuchAlgorithmException 没有这样的加密算法
+     * @throws UnsupportedEncodingException 解码异常
+     */
+    public String encrptyPassword(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        //获取使用哪种算法加密
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        //BASE64Encoder 防止存进数据库的是乱码
+        BASE64Encoder base64Encoder = new BASE64Encoder();
+        //对字符串进行加密
+        String result = base64Encoder.encode(md5.digest(password.getBytes("utf-8")));
+        return result;
+    }
+
+    /**
+     * 校验加密后的密码和数据库密码是否一致
+     * @param inputPwd 输入的密码
+     * @param dbPwd 数据库的密码
+     * @return
+     * @throws UnsupportedEncodingException
+     * @throws NoSuchAlgorithmException
+     */
+    public boolean cheackPassword(String inputPwd, String dbPwd) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        String result = encrptyPassword(inputPwd);
+        if (result.equals(dbPwd)) {
+            return true;
+        } else {
+            return  false;
+        }
+    }
+}
--- src/main/java/com/coding/login/controller/LoginController.java
+++ src/main/java/com/coding/login/controller/LoginController.java
@@ -2,16 +2,34 @@ package com.coding.login.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by 没想法的岁月 on 2018/4/27.
  */
 @Controller
-@RequestMapping("login")
+@RequestMapping("/login")
 public class LoginController {
     @RequestMapping
     public  String login(){
         return "login";
+    }
+
+    @RequestMapping("/check")
+    //返回的是主体信息而不是url
+    @ResponseBody
+    public String checkLogin(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String pwd = request.getParameter("password");
+
+        //查询数据库
+        //校验成功
+        //用户存session
+        //进入首页
+        //失败 提示
+        return "sucess";
     }
 }
 
