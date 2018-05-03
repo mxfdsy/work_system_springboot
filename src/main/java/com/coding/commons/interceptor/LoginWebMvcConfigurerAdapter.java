6e440e37da94de167702a644196af7433068c19d
963559210@qq.com <963559210@qq.com>
2018-05-03 12:34:49 +0800

    不写拦截器

--- src/main/java/com/coding/commons/interceptor/LoginWebMvcConfigurerAdapter.java
+++ src/main/java/com/coding/commons/interceptor/LoginWebMvcConfigurerAdapter.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author  没想法的岁月
  * @Date 2018/5/2 14:46
@@ -14,6 +17,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class LoginWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/**").excludePathPatterns("resources/static/**");
+        //静态资源拦截 不会写
+//      registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/static/jquery/**.js","/static/**");
     }
 }
