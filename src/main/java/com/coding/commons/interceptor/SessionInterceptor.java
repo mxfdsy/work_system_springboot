aa3b9dd333581dbc659a91b3b59b3c99868fe3b0
963559210@qq.com <963559210@qq.com>
2018-05-08 18:47:53 +0800

    工作流

--- src/main/java/com/coding/commons/interceptor/SessionInterceptor.java
+++ src/main/java/com/coding/commons/interceptor/SessionInterceptor.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * @author  没想法的岁月
+ * @author 没想法的岁月
  * @Date 2018/5/2 10:02
  * @Description Session 拦截器
  */
@@ -18,16 +18,16 @@ public class SessionInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String URI = request.getRequestURI();
-        if (URI.indexOf("login")>= 0) {
+        if (URI.indexOf("login") >= 0) {
             return true;
         }
         HttpSession session = request.getSession();
-        User user = (User)session.getAttribute("useriunfo");
+        User user = (User) session.getAttribute("useriunfo");
         if (user != null) {
             return true;
         }
         //跳转到的登录页面
-        request.getRequestDispatcher("/login").forward(request,response);
+        request.getRequestDispatcher("/login").forward(request, response);
         return false;
     }
 
